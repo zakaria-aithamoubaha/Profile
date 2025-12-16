25734c77c48f3be3c11f146458686b05677182d2
zakaria-aithamoubaha <aithamoubahazakaria@gmail.com>
2025-12-16 21:40:07 +0100

    Change base path for production in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === "development" ? "/" : process.env.VITE_BASE_PATH || "/",
+  base: process.env.NODE_ENV === "development" ? "/" : "/Profile/",
   optimizeDeps: {
     entries: ["src/main.tsx", "src/tempobook/**/*"],
   },
